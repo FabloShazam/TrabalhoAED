45f08255afbbae79e5bb9cb20048641ed1a29248
gabri <gabri@DESKTOP-KTQSS7H>
2020-06-17 00:11:46 -0300

    Merge sort completo

--- src/trabalhomodificado/GUT.java
+++ src/trabalhomodificado/GUT.java
@@ -1,11 +1,12 @@
 package trabalhomodificado;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class GUT {
 
-    private int[] MultGut;
-    private String[] Descricao;
+    public int[] MultGut;
+    public String[] Descricao;
 
     public void setTask() {
         Scanner t = new Scanner(System.in);
@@ -32,10 +33,52 @@ public class GUT {
 
         }
 
+        int[] v = {7, 8, 4, 1, 2, 5, 3, 6};
+        int[] w = new int[MultGut.length];
+
+        mergeSort(MultGut, w, 0, MultGut.length - 1);
+        System.out.println(Arrays.toString(MultGut));
+
     }
 
-    
-    //https://www.youtube.com/watch?v=5prE6Mz8Vh0 _ Favor assistir esse video para entender melhor essa parte
+    public static void mergeSort(int[] v, int[] w, int inicio, int fim) {
+
+        if (inicio < fim) {
+            int meio = (inicio + fim) / 2;
+            mergeSort(v, w, inicio, meio);
+            mergeSort(v, w, meio + 1, fim);
+            merge(v, w, inicio, meio, fim);
+        }
+    }
+
+    private static void merge(int[] v, int[] w, int inicio, int meio, int fim) {
+
+        for (int k = inicio; k <= fim; k++) {
+            w[k] = v[k];
+        }
+
+        int i = inicio;
+        int j = meio + 1;
+
+        for (int k = inicio; k <= fim; k++) {
+            if (i > meio) {
+                v[k] = w[j++];
+            } else if (j > fim) {
+                v[k] = w[i++];
+            } else if (w[i] < w[j]) {
+                v[k] = w[i++];
+            } else {
+                v[k] = w[j++];
+            }
+
+        }
+
+    }
+
+}
+
+//https://www.youtube.com/watch?v=5prE6Mz8Vh0 _ Favor assistir esse video para entender melhor essa parte
+/*
     public int[] MergeSort(int[] vet, int inicio, int fim) {
         if (fim - inicio > 1) {
             int medio = (inicio + fim) / 2;
@@ -80,4 +123,5 @@ public class GUT {
             }
         }
     }
-}
+   
+ */
